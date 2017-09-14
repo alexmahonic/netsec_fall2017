891f9b670d0d67c0ac86bc8d68e57b79d81587d4
alexmahonic <31637121+alexmahonic@users.noreply.github.com>
2017-09-13 23:19:05 -0400

    version1

---
+++ lab_1d/Client.py
@@ -0,0 +1,109 @@
+from asyncio import *
+from playground.network.packet import PacketType
+from playground.network.packet.fieldtypes import UINT32, STRING, BUFFER, BOOL
+from playground.asyncio_lib.testing import TestLoopEx
+from playground.network.testing import MockTransportToStorageStream
+from playground.network.testing import MockTransportToProtocol
+
+
+class RequestLogin(PacketType):
+    DEFINITION_IDENTIFIER = "lab1c.student_alex1.MyPacket"
+    DEFINITION_VERSION = "2.0"
+
+    FIELDS = [
+        ("LoginRequest", BOOL)
+    ]
+
+
+class IdentifyInfo(PacketType):
+    DEFINITION_IDENTIFIER = "lab1c.student_alex2.MyPacket"
+    DEFINITION_VERSION = "2.0"
+
+    FIELDS = [
+        ("pin", UINT32),
+        ("IDRequest", STRING),
+        ("PSWRequest", STRING)
+    ]
+
+
+class Answer(PacketType):
+    DEFINITION_IDENTIFIER = "lab1c.student_alex3.MyPacket"
+    DEFINITION_VERSION = "2.0"
+
+    FIELDS = [
+        ("pin", UINT32),
+        ("ID", STRING),
+        ("PSW", STRING)
+    ]
+
+
+class Result(PacketType):
+    DEFINITION_IDENTIFIER = "lab1c.student_alex4.MyPacket"
+    DEFINITION_VERSION = "2.0"
+
+    FIELDS = [
+        ("pin", STRING),
+        ("PassOrFail", BOOL)
+    ]
+
+
+class EchoClientProtocol(Protocol):
+    def __init__(self, loop):
+        # if callback:
+        #     self.callback = callback
+        # self.msg = msg
+        self.loop = loop
+        self.transport = None
+        self.rl = RequestLogin()
+        self.rl.LoginRequest = True
+
+        # self.ii = IdentifyInfo()
+        # self.ii.pin = 123
+        # self.ii.IDRequest = "Require ID"
+        # self.ii.PSWRequest = "Require psw"
+
+        self.ans = Answer()
+
+        # self.res = Result()
+        # self.res.pin = 123
+        # self.res.PassOrFail = True
+
+        self.deserializer = PacketType.Deserializer()
+
+    def connection_made(self, transport):
+        print("Client connects to Server...")
+        self.transport = transport
+        print("Client: I want to Log in.")
+        self.transport.write(self.msg.__serialize__())
+
+    def data_received(self, data):
+        self.deserializer.update(data)
+        for pkt in self.deserializer.nextPackets():
+            if isinstance(pkt, IdentifyInfo):
+                print("Client: Here is my information")
+                self.transport.write(self.ans.__serialize__())
+            elif isinstance(pkt, Result):
+                if pkt.PassOrFail:
+                    print("Client: Great!")
+                else:
+                    print("Client: Oops!")
+
+    def connection_lost(self, exc):
+        self.transport = None
+        print("The Server stopped and the loop stopped")
+
+    def SendLoginRequest(self):
+        self.transport.write(self.rl.__serialize__())
+
+    def SetIdentityInfo(self, pin, ID, PSW):
+        self.ans.pin = pin
+        self.ans.ID = ID
+        self.ans.PSW = PSW
+
+
+if __name__ == "__main__":
+    loop = get_event_loop()
+    coro = loop.create_connection(lambda: EchoClientProtocol(loop).SendLoginRequest(), '20174.0.0.0', 8000)
+    loop.run_until_complete(coro)
+    loop.run_forever()
+    loop.close()
---
+++ lab_1d/Server.py
@@ -0,0 +1,111 @@
+from asyncio import *
+from playground.network.packet import PacketType
+from playground.network.packet.fieldtypes import UINT32, STRING, BUFFER, BOOL
+from playground.asyncio_lib.testing import TestLoopEx
+from playground.network.testing import MockTransportToStorageStream
+from playground.network.testing import MockTransportToProtocol
+
+
+class RequestLogin(PacketType):
+    DEFINITION_IDENTIFIER = "lab1c.student_a1.MyPacket"
+    DEFINITION_VERSION = "3.0"
+
+    FIELDS = [
+        ("LoginRequest", BOOL)
+    ]
+
+
+class IdentifyInfo(PacketType):
+    DEFINITION_IDENTIFIER = "lab1c.student_a2.MyPacket"
+    DEFINITION_VERSION = "3.0"
+
+    FIELDS = [
+        ("pin", UINT32),
+        ("IDRequest", STRING),
+        ("PSWRequest", STRING)
+    ]
+
+
+class Answer(PacketType):
+    DEFINITION_IDENTIFIER = "lab1c.student_a3.MyPacket"
+    DEFINITION_VERSION = "3.0"
+
+    FIELDS = [
+        ("pin", UINT32),
+        ("ID", STRING),
+        ("PSW", STRING)
+    ]
+
+
+class Result(PacketType):
+    DEFINITION_IDENTIFIER = "lab1c.student_a4.MyPacket"
+    DEFINITION_VERSION = "3.0"
+
+    FIELDS = [
+        ("pin", STRING),
+        ("PassOrFail", BOOL)
+    ]
+
+
+class EchoServerProtocol(Protocol):
+    def __init__(self):
+        self.transport = None
+        # self.rl = RequestLogin()
+        # self.rl.LoginRequest = True
+
+        self.ii = IdentifyInfo()
+        self.ii.pin = 123
+        self.ii.IDRequest = "Require ID"
+        self.ii.PSWRequest = "Require psw"
+
+        self.ans = Answer()
+        self.ans.pin = 123
+        self.ans.ID = "Alex"
+        self.ans.PSW = "2017alex"
+
+        self.res = Result()
+        self.res.pin = 123
+        # self.res.PassOrFail = True
+
+        self.deserializer = PacketType.Deserializer()
+
+    def connection_made(self, transport):
+        # print("Server Connected to client...")
+        peername = transport.get_extra_info("peername")
+        print('Connection from {}'.format(peername))
+        self.transport = transport
+
+    def data_received(self, data):
+        self.deserializer.update(data)
+        for pkt in self.deserializer.nextPackets():
+            if isinstance(pkt, RequestLogin):
+                print("Server: Please provide Login Information")
+                self.transport.write(self.ii.__serialize__())
+            elif isinstance(pkt, Answer):
+                if pkt.pin == self.ans.pin and pkt.ID == self.ans.ID and pkt.PSW == self.ans.PSW:
+                    print("Server: Login Successfully!")
+                    self.res.PassOrFail = True
+                    self.transport.write(self.res.__serialize__())
+                else:
+                    print("Server: Authentication denied!")
+                    self.res.PassOrFail = False
+                    self.transport.write(self.res.__serialize__())
+                    # else: print("Login fails")
+                    # self.transport = None
+
+    def connection_lost(self, exc):
+        print("Connection lost!")
+
+if __name__ == "__main__":
+    loop = get_event_loop()
+    coro = loop.create_server(EchoServerProtocol, '20174.0.0.0', 8000)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_close())
+    loop.close()
